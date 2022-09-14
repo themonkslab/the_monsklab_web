1811994a90d3bb559b0734ed525ebb93392e41e9
Mauro Di Bert <maurodibert@gmail.com>
2022-09-14 08:55:57 -0300

    start with Archive - wip

--- lib/src/features/archive/presentation/view.dart
+++ lib/src/features/archive/presentation/view.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:the_monkslab_web/src/ui/_index.dart';
+import 'package:the_monkslab_web/src/utils/extensions.dart';
 
 class ArchiveView extends StatelessWidget {
   const ArchiveView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AppResponsiveScaffold(child: Container());
+    final screenType = context.getScreenType();
+
+    return AppResponsiveScaffold(
+        child: SingleChildScrollView(
+      child: screenType == ScreenType.phone || screenType == ScreenType.tablet
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('The Archive', style: AppTextStyles.h1HeaderPhone),
+                //TODO -CONT-: add description as constant
+              ],
+            )
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('The Archive', style: AppTextStyles.h1Header),
+              ],
+            ),
+    ));
   }
 }
