637f24b9c7e9e1eb195d2b577da06cc9f9652a6d
Mauro Di Bert <maurodibert@gmail.com>
2022-09-21 09:16:19 -0300

    add const

--- lib/src/features/home/presentation/view.dart
+++ lib/src/features/home/presentation/view.dart
@@ -23,7 +23,7 @@ class HomeView extends StatelessWidget {
             text: appTitle.toUpperCase(),
             textAlign: TextAlign.center,
             style: AppTextStyles.h1.copyWith(color: AppColors.white),
-            enabledDisabledColors: [AppColors.black, AppColors.white],
+            enabledDisabledColors: const [AppColors.black, AppColors.white],
             onPressed: () => context.beamToNamed('/${AppRoutes.archive.name}'),
           )
         ],
