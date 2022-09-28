0fcf330e9e1de7d7b48040d929529890a8046cfa
Mauro Di Bert <maurodibert@gmail.com>
2022-09-28 08:26:37 -0300

    add authentication bloc, event and state

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -1,13 +1,13 @@
 import 'dart:async';
 
-enum AuthenticationSatus { unknown, authenticated, unauthenticated }
+enum AuthenticationStatus { unknown, authenticated, unauthenticated }
 
 class AuthenticationRepository {
-  final _controller = StreamController<AuthenticationSatus>();
+  final _controller = StreamController<AuthenticationStatus>();
 
-  Stream<AuthenticationSatus> get status async* {
+  Stream<AuthenticationStatus> get status async* {
     await Future<void>.delayed(const Duration(seconds: 1));
-    yield AuthenticationSatus.unauthenticated;
+    yield AuthenticationStatus.unauthenticated;
     yield* _controller.stream;
   }
 
@@ -17,12 +17,12 @@ class AuthenticationRepository {
   }) async {
     await Future.delayed(
       Duration(milliseconds: 300),
-      () => _controller.add(AuthenticationSatus.authenticated),
+      () => _controller.add(AuthenticationStatus.authenticated),
     );
   }
 
   void logOut() {
-    _controller.add(AuthenticationSatus.unauthenticated);
+    _controller.add(AuthenticationStatus.unauthenticated);
   }
 
   void dispose() => _controller.close();
