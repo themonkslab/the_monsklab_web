59617619e8eee2672a3d5a7c48fa5a8720ce61fa
Mauro Di Bert <maurodibert@gmail.com>
2022-11-09 07:44:25 -0300

    create github_api package and transfer models

---
+++ package/github_api/lib/src/models/article.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:github_api/src/models/_index.dart';
+
+part 'article.freezed.dart';
+part 'article.g.dart';
+
+@freezed
+class Article with _$Article {
+  const factory Article({
+    required int id,
+    required String title,
+    required String description,
+    required String content,
+    required Author author,
+    required DateTime published,
+    String? readingTime,
+  }) = _Article;
+
+  factory Article.fromJson(Map<String, Object?> json) =>
+      _$ArticleFromJson(json);
+}
---
+++ package/github_api/lib/src/models/course.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'course.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Course _$$_CourseFromJson(Map<String, dynamic> json) => _$_Course(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      subTitle: json['subTitle'] as String?,
+      description: json['description'] as String,
+      authors: (json['authors'] as List<dynamic>)
+          .map((e) => Author.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      materialsAuthor: json['materialsAuthor'] as String?,
+      sections: (json['sections'] as List<dynamic>)
+          .map((e) => Section.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$_CourseToJson(_$_Course instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'subTitle': instance.subTitle,
+      'description': instance.description,
+      'authors': instance.authors,
+      'materialsAuthor': instance.materialsAuthor,
+      'sections': instance.sections,
+    };
