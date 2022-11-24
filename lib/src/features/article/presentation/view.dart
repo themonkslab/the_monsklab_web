29b70ca478a6c3eeaae2bae44f67d44ee1da9f80
Mauro Di Bert <maurodibert@gmail.com>
2022-11-24 08:51:00 -0300

    solve responsiveness on article

--- lib/src/features/article/presentation/view.dart
+++ lib/src/features/article/presentation/view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_monkslab_web/src/features/article/_index.dart';
 import 'package:the_monkslab_web/src/repositories/courses_repository.dart';
 import 'package:the_monkslab_web/src/ui/_index.dart';
+import 'package:the_monkslab_web/src/utils/_index.dart';
 
 import 'bloc/article_cubit.dart';
 
@@ -61,91 +62,70 @@ class _ArticleViewState extends State<ArticleView> {
       builder: ((context, state) {
         screenHeight = MediaQuery.of(context).size.height;
 
-        final article = state.article;
-        // TODO use build.when?
+        //TODO -MEDIUM- use build.when?
         switch (state.status) {
           case ArticleStatus.loading:
             return const AppLoader();
           case ArticleStatus.failure:
             return const AppFailure();
           case ArticleStatus.success:
-            return Scaffold(
-              appBar: isHeaderOnScreen
-                  ? const AppAppBar(
-                      elevation: 0,
-                      showSocials: false,
-                      backgroundColor: AppColors.black,
-                    )
-                  : const AppAppBar(showSocials: true),
-              //TODO -HIGH- make all screens work
-              body: ListView(
-                controller: scrollController,
-                children: <Widget>[
-                  ArticleHeader(chapter: article!),
-                  AppGaps.gapH24,
-                  ArticleBody(data: article.content),
-                  AppGaps.gapH48,
-                ],
-              ),
-            );
+            final article = state.article;
+            final screenType = context.getScreenType();
+            final isDesktopOrLarge = screenType == ScreenType.desktop ||
+                screenType == ScreenType.large;
+
+            if (screenType == ScreenType.phone) {
+              return Scaffold(
+                appBar: isHeaderOnScreen
+                    ? const AppAppBar(
+                        elevation: 0,
+                        showSocials: false,
+                        backgroundColor: AppColors.black,
+                      )
+                    : const AppAppBar(showSocials: true),
+                body: ListView(
+                  controller: scrollController,
+                  children: <Widget>[
+                    ArticleHeader(chapter: article!),
+                    AppGaps.gapH24,
+                    ArticleBody(data: article.content),
+                    AppGaps.gapH48,
+                  ],
+                ),
+              );
+            } else {
+              return Scaffold(
+                appBar: isHeaderOnScreen
+                    ? const AppAppBar(
+                        elevation: 0,
+                        showSocials: false,
+                        backgroundColor: AppColors.black,
+                      )
+                    : const AppAppBar(showSocials: true),
+                body: SingleChildScrollView(
+                    controller: scrollController,
+                    child: Column(
+                      children: [
+                        ArticleHeader(
+                          chapter: article!,
+                        ),
+                        AppGaps.gapH24,
+                        Container(
+                            color: Colors.white,
+                            width: isDesktopOrLarge
+                                ? AppSizes.largeContentContainer
+                                : AppSizes.desktopContentContainer,
+                            child: ArticleBody(data: article.content)),
+                        AppGaps.gapH48,
+                      ],
+                    )),
+              );
+            }
+
           default:
             return const AppFailure();
         }
       }),
     );
-    // screenHeight = MediaQuery.of(context).size.height;
-    // final chapter = flutterLearningPath.courses[widget.courseId]
-    //     .sections[widget.sectionId].chapters[widget.chapterId];
-
-    // final screenType = context.getScreenType();
-    // final isDesktopOrLarge =
-    //     screenType == ScreenType.desktop || screenType == ScreenType.large;
-
-    // if (screenType == ScreenType.phone) {
-    //   return Scaffold(
-    //     appBar: isHeaderOnScreen
-    //         ? const AppAppBar(
-    //             elevation: 0,
-    //             showSocials: false,
-    //             backgroundColor: AppColors.black,
-    //           )
-    //         : const AppAppBar(showSocials: true),
-    //     body: ListView(
-    //       controller: scrollController,
-    //       children: <Widget>[
-    //         ArticleHeader(chapter: chapter),
-    //         AppGaps.gapH24,
-    //         ArticleBody(data: chapter.content),
-    //         AppGaps.gapH48,
-    //       ],
-    //     ),
-    //   );
-    // } else {
-    //   return Scaffold(
-    //     appBar: isHeaderOnScreen
-    //         ? const AppAppBar(
-    //             elevation: 0,
-    //             showSocials: false,
-    //             backgroundColor: AppColors.black,
-    //           )
-    //         : const AppAppBar(showSocials: true),
-    //     body: SingleChildScrollView(
-    //         controller: scrollController,
-    //         child: Column(
-    //           children: [
-    //             ArticleHeader(
-    //               chapter: chapter,
-    //             ),
-    //             AppGaps.gapH24,
-    //             Container(
-    //                 color: Colors.white,
-    //                 width: isDesktopOrLarge
-    //                     ? AppSizes.largeContentContainer
-    //                     : AppSizes.desktopContentContainer,
-    //                 child: ArticleBody(data: chapter.content)),
-    //             AppGaps.gapH48,
-    //           ],
-    //         )),
-    //   );
   }
 }
