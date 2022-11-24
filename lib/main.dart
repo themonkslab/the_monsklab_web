6ebac8e40f4618ae879aebaa0deaa4cf339c50bc
Mauro Di Bert <maurodibert@gmail.com>
2022-11-24 08:45:53 -0300

    solve web issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:the_monkslab_web/firebase_options.dart';
 import 'package:the_monkslab_web/src/app.dart';
 import 'package:the_monkslab_web/src/repositories/_index.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyBGOVRyABKzyN1sK8bfw95e1YaCY4aQrCw',
+      appId: '1:837553242957:web:5d207458020ebd1bedad04',
+      messagingSenderId: '837553242957',
+      projectId: 'monsklab-courses',
+      authDomain: 'monsklab-courses.firebaseapp.com',
+      storageBucket: 'monsklab-courses.appspot.com',
+      measurementId: 'G-T4QMR5W0FB',
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(App(coursesRepository: CoursesRepository()));
 }
