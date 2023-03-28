afed773144c3094b90325a8f12816c6af3470aae
eugenio-d-tesio <eugenio@pairtreefamily.com>
2023-03-28 08:34:14 -0300

    fix test

--- test/src/features/archive/archive_robot.dart
+++ test/src/features/archive/archive_robot.dart
@@ -11,14 +11,17 @@ class ArchiveRobot {
   final WidgetTester tester;
 
   Future<void> pumpWidgetWithRepository(
-      CoursesRepository coursesRepository,) async {
-    await tester.pumpWidget(RepositoryProvider.value(
-      value: coursesRepository,
-      child: MaterialApp.router(
-        routeInformationParser: BeamerParser(),
-        routerDelegate: routerDelegate(initialPath: '/archive'),
+    CoursesRepository coursesRepository,
+  ) async {
+    await tester.pumpWidget(
+      RepositoryProvider.value(
+        value: coursesRepository,
+        child: MaterialApp.router(
+          routeInformationParser: BeamerParser(),
+          routerDelegate: routerDelegate,
+        ),
       ),
-    ),);
+    );
   }
 
   Future<void> pumpWidgetWithProvider({
