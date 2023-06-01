ca8662d49155d19b47aa3e8a6a70aebdcd360183
eugenio-d-tesio <eugenio@pairtreefamily.com>
2023-06-01 14:07:33 -0300

    fix: broken tests

--- lib/src/features/home/presentation/widgets/hire_us.dart
+++ lib/src/features/home/presentation/widgets/hire_us.dart
@@ -14,7 +14,7 @@ class HireUs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+      height: 410,
       width: double.infinity,
       decoration: BoxDecoration(
         color: AppColors.secondaryLight,
